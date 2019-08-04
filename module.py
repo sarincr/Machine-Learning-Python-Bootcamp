a02a35255496986112b2970e08da59330d22d019
Sarin CR <sarincrg@gmail.com>
2019-08-04 17:29:59 +0530

    Modules

---
+++ module.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def greeting(name):
+  print("Hello, " + name) 
+
---
+++ modules.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
